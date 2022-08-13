aa806ec63b6121a8ba95d11569ef7e25bbeed327
Clément Théraud <theraud.clement@gmail.com>
2022-08-13 23:03:22 +0200

    start

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Router from './components/Router';
+import store from './utils/store'
+import { Provider } from 'react-redux'
+import "./style/index.scss"
+import { faCircleUser, faSignOut, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+// Global FontAwesome icons library
+library.add(faCircleUser, faSignOut, faArrowLeft)
+
+// App router(createRoot) and Redux Store Provider
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+    <Provider store={store}>
+            <Router tab="home" />
+    </Provider>
+);
