4aaf95e2d133505b4a5fd0da0389f74b251decb0
Clément Théraud <theraud.clement@gmail.com>
2022-09-06 09:36:30 +0200

    fontawesome correct
    reinstall fawsm lib

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client';
 import Router from './components/Router';
 import store from './utils/store'
 import { Provider } from 'react-redux'
+
 import "./style/index.css"
-import { faCircleUser, faSignOut, faArrowLeft } from '@fortawesome/free-solid-svg-icons' //fontawesome?
-import { library } from '@fortawesome/fontawesome-svg-core'; //fontawesome?
+import { faCircleUser, faSignOut, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core'; 
 
 // Global FontAwesome icons library
 library.add(faCircleUser, faSignOut, faArrowLeft)
