a4ea11420cdfaf1cf6ad0dcf44f27678896c54d0
Clément Théraud <theraud.clement@gmail.com>
2022-09-06 11:41:23 +0200

    scss
    synt modif home js
    modif scss form sign in

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -40,14 +40,14 @@ const Home = () => {
             <div className="hero" style={{ backgroundImage: `url(${backgroundImage})`}}>
                 <section className="content">
                     <h2 className="sr-only">Promoted Content</h2>
-                    <p class="subtitle">No fees.</p>
-                    <p class="subtitle">No minimum deposit.</p>
-                    <p class="subtitle">High interest rates.</p>
-                    <p class="text">Open a savings account with Argent Bank today!</p>
+                    <p className="subtitle">No fees.</p>
+                    <p className="subtitle">No minimum deposit.</p>
+                    <p className="subtitle">High interest rates.</p>
+                    <p className="text">Open a savings account with Argent Bank today!</p>
                 </section>
             </div>
-            <section class="features">
-            <h2 class="sr-only">Features</h2>
+            <section className="features">
+            <h2 className="sr-only">Features</h2>
             {features.map((feature, index) => (
                 <Feature value={feature} key={`feature-${index}`} />
             ))}
