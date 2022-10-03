e990fecad21525fe990cfee498dc8d0b20bf1289
Clément Théraud <theraud.clement@gmail.com>
2022-10-03 11:45:14 +0200

    Update store.js

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,12 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userIdReducer from './slice/userIdSlice'
 
-// Pour connecter les Redux Devtools on utilise
-// un fonction disponible sur l'objet window
-// Si cette fonction existe on l'exécute.
-const reduxDevtools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  
 export default configureStore({
     reducer: {
         user: userIdReducer,
